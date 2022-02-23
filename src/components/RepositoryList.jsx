cd6401a9128447cfd12a052c7f04d160ea394dbc
ThallesLana <thalles.lana1@gmail.com>
2022-02-23 01:26:33 -0300

    Utilizando o hook useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
@@ -9,6 +12,17 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    // useEffect serve para disparar uma função após uma alteração
+    // 1° parametro o que eu quero executar, quando eu quero executar
+    // 2° parametro caso retornado [] ele só roda uma vez
+    // cuidado para não esquecer do segundo parametro
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json()).then(data => setRepositories(data));
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
